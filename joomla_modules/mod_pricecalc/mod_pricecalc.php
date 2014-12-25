bea43cba8dae03b3c791ec54e5ac3aa60727eaf6
Yanxin Gong <yanxin.gong22@gmail.com>
2014-12-25 22:44:56 +0100

    新建joomla_modules文件夹

---
+++ joomla_modules/mod_pricecalc/mod_pricecalc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Module's entry point
+ * 
+ * @package    com.express
+ * @subpackage Modules
+ * @license    ???
+ */
+
+defined('_JEXEC') or die;
+
+require_once( dirname(__FILE__) . '/jHelper.php' );
+ 
+$priceCalcInfo = modPriceCalcHelper::getPrice($params);
+require( JModuleHelper::getLayoutPath('mod_pricecalc'));
+
+?>
