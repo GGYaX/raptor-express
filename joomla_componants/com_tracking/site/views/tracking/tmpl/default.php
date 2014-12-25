9ba9ae5c98dc8b3aad6dbec844e6cab7f2583b56
Yanxin Gong <yanxin.gong22@gmail.com>
2014-12-25 22:36:21 +0100

    添加joomla_componants文件夹

---
+++ joomla_componants/com_tracking/site/views/tracking/tmpl/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @package Joomla.Administrator
+* @subpackage com_tracking
+*
+* @copyright Copyright (C) 2014 
+* @license GNU??? BSD???
+*/
+defined('_JEXEC') or die;
+?>
+
+<h1><?php echo $this->msg; ?></h1> 
