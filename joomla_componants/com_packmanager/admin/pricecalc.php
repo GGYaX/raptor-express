090b264d594d72bc55ddbf941f45df23950ba1ff
MWCHEN <900930@gmail.com>
2014-12-29 23:50:52 +0100

    杂碎

---
+++ joomla_componants/com_packmanager/admin/pricecalc.php
@@ -0,0 +1,3 @@
+<?php
+  echo "Price Simulator back-end : nothing to do here for instance...";
+?>
---
+++ joomla_componants/com_packmanager/site/controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @package Joomla.Administrator
+* @subpackage com_pricecalc
+*
+* @copyright Copyright (C) 2014 
+* @license GNU??? BSD???
+*/
+
+defined('_JEXEC') or die;
+jimport('joomla.application.component.view');
+ 
+/**
+* @since 1.0.0
+*/
+class PriceCalcController extends JControllerLegacy
+{
+}
