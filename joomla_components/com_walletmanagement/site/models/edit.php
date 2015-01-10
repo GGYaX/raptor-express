3ee573009dcc3ad11f2e358dda132d4486190b90
Yanxin Gong <yanxin.gong22@gmail.com>
2015-01-10 22:25:16 +0100

    com_walletmanagement修改select语句，mysql lower_case_table_names  = 2 in linux

--- joomla_components/com_walletmanagement/site/models/edit.php
+++ joomla_components/com_walletmanagement/site/models/edit.php
@@ -108,7 +108,7 @@ class WalletmanagementModelEdit extends JModelItem
         try {
 
             $db = JFactory::getDBO();
-            $query = 'INSERT INTO T_BALANCE_MODIFICATIONS (`amount`, `wallet_id`, `wallet_type`, `date`, `comment`) VALUES (' .
+            $query = 'INSERT INTO t_balance_modifications (`amount`, `wallet_id`, `wallet_type`, `date`, `comment`) VALUES (' .
                      $db->quote($amount) . ',' . $db->quote($walletId) . ',' .
                      $db->quote($walletType) . ',' . 'NOW()' . ',' .
                      $db->quote($comment) . ');';
